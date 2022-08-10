b209f09bf976557d5df33789f2df1faeb3b38875
Arturo Macias <arturomaciasmx@gmail.com>
2022-08-10 13:29:43 -0700

    Nested screens

--- src/screens/Restaurant.tsx
+++ src/screens/Restaurant.tsx
@@ -1,13 +1,27 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { StyleSheet, Text, View } from "react-native";
+import RestaurantCard from "../atoms/RestaurantCard";
 import { DefaultStackParams } from "../navigators/DefaultStack";
 
 type Props = NativeStackScreenProps<DefaultStackParams, "Restaurant">;
 
-const Restaurant = ({ route }: Props) => {
+const Restaurant = ({ route, navigation }: Props) => {
   return (
     <View>
       <Text>{route.params.name}</Text>
+      <Text style={styles.title}>Related Restaurants</Text>
+      <RestaurantCard
+        name="Related restaurant 1"
+        onPress={(name) => navigation.push("Restaurant", { name })}
+      />
+      <RestaurantCard
+        name="Related restaurant 2"
+        onPress={(name) => navigation.push("Restaurant", { name })}
+      />
+      <RestaurantCard
+        name="Related restaurant 3"
+        onPress={(name) => navigation.push("Restaurant", { name })}
+      />
     </View>
   );
 };
